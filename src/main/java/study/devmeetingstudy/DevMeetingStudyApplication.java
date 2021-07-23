d3e8a6c29c06b77e22f6663d636210da188b8222
jinseok <stay0073@gmail.com>
2021-07-23 11:45:37 +0900

    spring boot

---
+++ src/main/java/study/devmeetingstudy/DevMeetingStudyApplication.java
@@ -0,0 +1,13 @@
+package study.devmeetingstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevMeetingStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevMeetingStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/devmeetingstudy/DevMeetingStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.devmeetingstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevMeetingStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
