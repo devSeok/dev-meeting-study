af8e76a3b1f7465ce5b36e9c1ade50f7e9676737
jinseok <stay0073@gmail.com>
2021-07-26 21:53:19 +0900

    jwt 토큰 수정

--- src/main/java/study/devmeetingstudy/service/AuthService.java
+++ src/main/java/study/devmeetingstudy/service/AuthService.java
@@ -40,16 +40,11 @@ public class AuthService {
     public TokenDto login(MemberRequestDto memberRequestDto) {
         // 1. Login ID/PW 를 기반으로 AuthenticationToken 생성
         UsernamePasswordAuthenticationToken authenticationToken = memberRequestDto.toAuthentication();
-
-        System.out.println(memberRequestDto.toAuthentication().toString());
-        System.out.println("===============================authenticationToken===============================");
+        
         // 2. 실제로 검증 (사용자 비밀번호 체크) 이 이루어지는 부분
         //    authenticate 메서드가 실행이 될 때 CustomUserDetailsService 에서 만들었던 loadUserByUsername 메서드가 실행됨
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
 
-        System.out.println("authentication ========" + authentication);
-
-        System.out.println("===============================authentication===============================");
         // 3. 인증 정보를 기반으로 JWT 토큰 생성
         TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
 
