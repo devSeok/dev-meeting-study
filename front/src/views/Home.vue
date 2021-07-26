058f175a13d311e6218e8750a099c389b58b6d55
miocat <kkm1640@naver.com>
2021-07-26 21:41:36 +0900

    scss setting

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -16,3 +16,7 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 })
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss">
+@import "@/assets/styles/index";
+</style>
