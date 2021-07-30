3367efaef29d6c77fb4b4ab97305ce73e57c263a
miocat2876 <73389799+miocat2876@users.noreply.github.com>
2021-07-30 09:02:27 +0900

    Add files via upload

---
+++ src/main/java/study/devmeetingstudy/config/CorsConfig.java
@@ -0,0 +1,15 @@
+package com.streetflo.miocat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") //모든 요청에 대해서
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST");
+    }
+}
