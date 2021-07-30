2869c052c21cf535d0a76de9018f26750a04c7d6
miocat2876 <73389799+miocat2876@users.noreply.github.com>
2021-07-30 10:24:44 +0900

    Update ServletInitializer.java
    외장 톰캣 설정

--- src/main/java/study/devmeetingstudy/ServletInitializer.java
+++ src/main/java/study/devmeetingstudy/ServletInitializer.java
@@ -1,4 +1,5 @@
-package com.streetflo.miocat;
+  
+package study.devmeetingstudy;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +8,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(MiocatApplication.class);
+        return application.sources(DevMeetingStudyApplication.class);
     }
 
 }
