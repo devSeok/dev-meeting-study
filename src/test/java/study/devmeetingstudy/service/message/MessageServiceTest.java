59dc5f6e44864e00f6966b8efc382c5d7a78b9c8
Lee SeungWoo <xonic789@naver.com>
2021-08-03 23:28:16 +0900

    Message 생성 테스트 케이스 작성

--- src/test/java/study/devmeetingstudy/service/message/MessageServiceTest.java
+++ src/test/java/study/devmeetingstudy/service/message/MessageServiceTest.java
@@ -1,17 +1,18 @@
 package study.devmeetingstudy.service.message;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 import study.devmeetingstudy.domain.Authority;
 import study.devmeetingstudy.domain.Member;
 import study.devmeetingstudy.domain.Message;
 import study.devmeetingstudy.domain.UserStatus;
+import study.devmeetingstudy.dto.message.MessageRequestDto;
 import study.devmeetingstudy.repository.MemberRepository;
 import study.devmeetingstudy.repository.message.MessageRepository;
 
@@ -31,28 +32,18 @@ class MessageServiceTest {
     @Autowired
     MemberRepository memberRepository;
 
-    /**
-     * 일단 em 으로 진행.
-     * @throws Exception
-     */
-    @Test
-    void 메시지_생성() throws Exception{
-        // given
-        Member member = buildMember("xonic@na.na","1234");
-        Member sender = buildMember("dltmddn@na.na", "1234");
+
+    Member member;
+    Member sender;
+
+    @BeforeEach
+    void setMemberAndSender(){
+        member = buildMember("xonic@na.na","1234");
+        sender = buildMember("dltmddn@na.na", "1234");
         em.persist(member);
         em.persist(sender);
-        Message message = getMessage(member,sender);
-        em.persist(message);
-
-        // when
         em.flush();
         em.clear();
-
-        Message findMessage = em.find(Message.class, 1L);
-
-        // then
-        Assertions.assertEquals(message, findMessage);
     }
 
     private Member buildMember(String email, String password) {
@@ -64,7 +55,7 @@ class MessageServiceTest {
                 .status(UserStatus.active).build();
     }
 
-    private Message getMessage(Member member, Member sender){
+    private Message buildMessage(Member member, Member sender){
         return Message.builder()
                 .senderId(sender.getId())
                 .content("1234")
@@ -73,7 +64,30 @@ class MessageServiceTest {
     }
 
 
+    @Test
+    void 메시지생성_객체가같은지_True(){
+        // given
 
+        Message memberToSenderMessage = messageService.sendMessage(getMessageDto(sender), member, sender);
+        Message senderToMemberMessage = messageService.sendMessage(getMessageDto(member), sender, member);
 
+        System.out.println(memberToSenderMessage);
+        System.out.println(senderToMemberMessage);
+        // when
+//        Message findMessage = em.find(Message.class, 1L);
+
+        // then
+//        Assertions.assertEquals(message, findMessage);
+    }
+
+    private MessageRequestDto getMessageDto(Member member){
+        if (this.member == member) member = this.member;
+        else member = this.sender;
+        return createMessage(member);
+    }
+
+    private MessageRequestDto createMessage(Member member){
+        return new MessageRequestDto(sender.getId(), "하이");
+    }
 
 }
