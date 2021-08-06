c4d388b84cce41c3b0b22ebd21408bffb0991e26
Lee SeungWoo <xonic789@naver.com>
2021-08-06 17:49:32 +0900

    패키지 경로 추가 및 의존성 추가 (모키토, h2,)

--- src/main/java/study/devmeetingstudy/controller/MessageController.java
+++ src/main/java/study/devmeetingstudy/controller/MessageController.java
@@ -1,2 +1,42 @@
-package study.devmeetingstudy.controller;public class MessageController {
+package study.devmeetingstudy.controller;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import study.devmeetingstudy.common.exception.global.response.ApiResponseDto;
+import study.devmeetingstudy.domain.Member;
+import study.devmeetingstudy.service.message.MessageService;
+
+import java.util.HashMap;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/messages")
+public class MessageController {
+    private final MessageService messageService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponseDto> sendMessage(){
+
+
+        return new ResponseEntity<ApiResponseDto>(
+                new ApiResponseDto(
+                        "성공",
+                        HttpStatus.OK.value(),
+                        new ResponseMessage()),
+                HttpStatus.OK);
+    }
+
+    @Data
+    class ResponseMessage{
+        private Member member;
+        private Member sender;
+        private String content;
+    }
+
 }
---
+++ src/main/java/study/devmeetingstudy/domain/message/enums/MessageStatus.java
@@ -0,0 +1,6 @@
+package study.devmeetingstudy.domain.message.enums;
+
+public enum MessageStatus {
+    READ,NOT_READ
+
+}
