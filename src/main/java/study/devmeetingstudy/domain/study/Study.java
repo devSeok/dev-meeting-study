84c603ad0e2ba1a14d4e177ba7ebaa24956321df
Lee SeungWoo <xonic789@naver.com>
2021-08-06 22:53:32 +0900

    hotfix 메시지 패키지 경로 수정

--- src/main/java/study/devmeetingstudy/domain/study/Study.java
+++ src/main/java/study/devmeetingstudy/domain/study/Study.java
@@ -7,6 +7,8 @@ import study.devmeetingstudy.domain.study.enums.StudyType;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
