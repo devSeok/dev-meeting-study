5ebb525070bf4672589ef61dc58a67a30bfe25ea
Lee SeungWoo <xonic789@naver.com>
2021-08-06 17:48:30 +0900

    패키지 경로 수정

---
+++ src/test/java/study/devmeetingstudy/controller/MessageControllerTest.java
@@ -0,0 +1,14 @@
+package study.devmeetingstudy.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MessageControllerTest {
+
+}
