c233c9073fe57d48c0a522558db028d55dd79473
xonic789 <xonic789@naver.com>
2021-08-25 23:01:40 +0900

    test !: Message status 수정 관련 테스트코드 작성
    DeletionStatus, MessageReadStatus 테스트 코드 작성

---
+++ src/test/java/study/devmeetingstudy/domain/message/MessageTest.java
@@ -0,0 +1,72 @@
+package study.devmeetingstudy.domain.message;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import study.devmeetingstudy.domain.member.Member;
+import study.devmeetingstudy.domain.member.enums.Authority;
+import study.devmeetingstudy.domain.member.enums.MemberStatus;
+import study.devmeetingstudy.domain.message.enums.MessageDeletionStatus;
+import study.devmeetingstudy.domain.message.enums.MessageReadStatus;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageTest {
+    private Member member;
+    private Member sender;
+
+    @BeforeEach
+    void injectField(){
+        member = createMember(1L, "dltmddn@na.na","nick1");
+        sender = createMember(2L, "sksk@sksk.sksk", "nick2");
+    }
+
+    Member createMember(Long id, String email, String nickname){
+        return Member.builder()
+                .authority(Authority.ROLE_USER)
+                .email(email)
+                .password("1234")
+                .status(MemberStatus.ACTIVE)
+                .grade(0)
+                .nickname(nickname)
+                .id(id)
+                .build();
+    }
+
+    @Test
+    void 메시지상태수정_수정후상태비교_True() throws Exception{
+        //given
+
+
+        Message message = getMessage();
+
+        //when
+        Message.changeReadStatus(message, MessageReadStatus.READ);
+
+        //then
+        assertEquals(MessageReadStatus.READ, message.getStatus());
+    }
+
+    private Message getMessage() {
+        Message message = Message.builder()
+                .id(1L)
+                .senderId(sender.getId())
+                .senderName(sender.getNickname())
+                .member(member)
+                .content("하이")
+                .delflg(MessageDeletionStatus.NOT_DELETED)
+                .status(MessageReadStatus.NOT_READ)
+                .build();
+        return message;
+    }
+
+    @Test
+    public void 메시지삭제처리_삭제후상태비교_True() throws Exception{
+        //given
+        Message message = getMessage();
+        //when
+        Message.changeDeletionStatus(message, MessageDeletionStatus.DELETED);
+        //then
+        assertEquals(MessageDeletionStatus.DELETED, message.getDelflg());
+    }
+
+}
