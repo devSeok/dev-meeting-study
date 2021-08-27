3695077b6f7d4b2f4a1f587c62ba117fe9328a2b
devSeok <stay0073@gmail.com>
2021-08-27 17:05:37 +0900

    fix : MemberResolverDto 파라미터값 초기화

--- src/main/java/study/devmeetingstudy/annotation/dto/MemberResolverDto.java
+++ src/main/java/study/devmeetingstudy/annotation/dto/MemberResolverDto.java
@@ -2,12 +2,17 @@ package study.devmeetingstudy.annotation.dto;
 
 
 import lombok.*;
+import study.devmeetingstudy.domain.member.Member;
 
-@AllArgsConstructor
+
 @Data
 public class MemberResolverDto {
 
     private Long id;
     private String nickname;
 
+    public MemberResolverDto(Member member) {
+        this.id = member.getId();
+        this.nickname = member.getNickname();
+    }
 }
