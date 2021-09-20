535770e790ac3859e455b7481a3b954134b43813
xonic789 <xonic789@naver.com>
2021-09-20 21:32:30 +0900

    feat !: study 관련 subject, address 테스트 코드 작성 및 수정

---
+++ src/test/java/study/devmeetingstudy/service/AddressServiceTest.java
@@ -0,0 +1,68 @@
+package study.devmeetingstudy.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import study.devmeetingstudy.domain.Address;
+import study.devmeetingstudy.dto.address.AddressRequestDto;
+import study.devmeetingstudy.repository.AddressRepository;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+class AddressServiceTest {
+
+    @InjectMocks
+    private AddressService addressService;
+
+    @Mock
+    private AddressRepository addressRepository;
+
+    @DisplayName("주소 저장")
+    @Test
+    void saveAddress() {
+        //given
+        AddressRequestDto addressRequestDto = new AddressRequestDto("경기도", "광주시", "오포읍");
+        Address expectedAddress = Address.create(addressRequestDto);
+        doReturn(expectedAddress).when(addressRepository).save(any(Address.class));
+
+        //when
+
+        Address saveAddress = addressService.saveAddress(addressRequestDto);
+
+        //then
+        assertEquals(expectedAddress, saveAddress);
+    }
+
+
+    @DisplayName("주소 조회")
+    @Test
+    void findAddress() {
+        //given
+        Long id = 1L;
+        Address expectedAddress = createAddress();
+        doReturn(Optional.of(expectedAddress)).when(addressRepository).findById(anyLong());
+        //when
+        Address address = addressService.findAddress(id);
+
+        //then
+        assertEquals(expectedAddress, address);
+    }
+
+    private Address createAddress(){
+        return Address.builder()
+                .id(1L)
+                .address1("경기도")
+                .address2("광주시")
+                .address3("오포읍")
+                .build();
+    }
+}
