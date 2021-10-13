ca0d50ad13b1cd65d7a2352c936258ee069de805
xonic789 <xonic789@naver.com>
2021-10-13 16:40:31 +0900

    fix !: /api/studies/** 권한 없이 접근 가능하게 수정

--- src/main/java/study/devmeetingstudy/config/security/WebSecurityConfig.java
+++ src/main/java/study/devmeetingstudy/config/security/WebSecurityConfig.java
@@ -68,6 +68,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                     .antMatchers("/api/auth/**").permitAll()
                     .antMatchers("/api/studies").permitAll()
+                    .antMatchers("/api/studies/**").permitAll()
                     .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
 
                 // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
