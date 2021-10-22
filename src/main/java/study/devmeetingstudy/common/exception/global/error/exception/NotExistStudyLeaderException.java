eaf15f6fabf5d6d6f0700f97aeb70296e2c123fd
xonic789 <xonic789@naver.com>
2021-10-22 17:08:31 +0900

    feat !: 스터디 소유자 관련 엑셉션 추가

--- src/main/java/study/devmeetingstudy/common/exception/global/error/exception/NotExistStudyLeaderException.java
+++ src/main/java/study/devmeetingstudy/common/exception/global/error/exception/NotExistStudyLeaderException.java
@@ -1,2 +1,7 @@
-package study.devmeetingstudy.common.exception;public class NotExistStudyLeaderException {
+package study.devmeetingstudy.common.exception.global.error.exception;
+
+public class NotExistStudyLeaderException extends BusinessException {
+    public NotExistStudyLeaderException(String message) {
+        super(message, ErrorCode.INTERNAL_SERVER_ERROR);
+    }
 }
