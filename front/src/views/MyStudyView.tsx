14c18432c4b376f37a2a857bf36cf056da857980
Hyunsungjun <bnmva23@hanmail.net>
2021-10-28 20:49:04 +0900

    fix: message undefined (reading 'data') 해결

--- front/src/views/MyStudyView.tsx
+++ front/src/views/MyStudyView.tsx
@@ -88,11 +88,11 @@ interface PayloadProps {
 function MyStudyView() {
   const Dispatch = useDispatch();
   console.log('요청 전');
-  const messageData: PayloadProps = useSelector(message);
+  // const messageData: PayloadProps = useSelector(message);
   // console.log("요청 후 ", messageData);
-  console.log('messageData', messageData);
-  const messageList: any = messageData.payload.payload.data;
-  console.log('messageList', messageList);
+  // console.log('messageData', messageData);
+  // const messageList: any = messageData.payload.payload.data;
+  // console.log('messageList', messageList);
 
   const history = useHistory();
   const [inputs, setInputs] = useState({
@@ -187,7 +187,7 @@ function MyStudyView() {
           <div style={{ marginTop: '50px' }}>
             {pathname === '/my/info' && <Info onChange={onChange} />}
             {pathname === '/my/study' && <Study study={study} index={item} />}
-            {pathname === '/my/message' && <Message post={messageList} index={item} />}
+            {pathname === '/my/message' && <Message index={item} />}
             {pathname === '/my/secession' && <Secession onChange={onChange} />}
           </div>
         </Section>
@@ -240,11 +240,11 @@ export function Study({ study, index }: PType) {
   );
 }
 
-export function Message({ post, index }: PType) {
-  console.log('post111111', post);
+export function Message({ index }: PType) {
+  // console.log('post111111', post);
   return (
     <>
-      <StudyColumnList items={post} index={index} />
+      <StudyColumnList index={index} />
     </>
   );
 }
